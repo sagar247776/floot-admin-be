f7491d6b52e5a42b347a42e46b24b7a2b799ef66
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-08-02 02:33:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://floot-admin.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
