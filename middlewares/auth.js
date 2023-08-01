a9966752e190d32017f07aa5dd75e304e0821355
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-08-02 03:08:51 +0530

    cookie fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,20 +10,3 @@ export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   req.user = await User.findById(decoded._id);
   next();
 });
-
-export const authorizeAdmin = (req, res, next) => {
-  if (req.user.role !== "admin")
-    return next(
-      new ErrorHandler(`${req.user.role} is not allowed this resource`, 403)
-    );
-  next();
-};
-
-
-export const authorizeSubscribers = (req, res, next) => {
-  if (req.user.subscription.status !== "active" && req.user.role !== "admin")
-    return next(
-      new ErrorHandler("Only Subscribers can access this resource", 403)
-    );
-  next();
-};
